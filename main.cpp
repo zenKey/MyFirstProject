40437f57bdf8e4e89c55583763cb2359afe2b137
zenKey <244557859@qq.com>
2018-12-02 16:47:04 +0800

    First Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C), 2018-2018
+ * FileName: main
+ * Author:   zenKey
+ * Date:     2018-12-02 16:30
+ * Description: Hello World！
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * zenKey           2018-12-02          1.0             Hello World！
+ */
+#include <iostream>
+int main(int argc, char *argv[]){
+    std::cout << "Hello World!" << argv[1] << std::endl;
+    return 0;
+
+}
